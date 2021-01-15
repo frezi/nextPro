90596c46cbc3aead6039f2e94b62460f25afbf20
庄燕玲(23094790) <zhuangyanling001@ke.com>
2021-01-15 09:58:18 +0800

    add-redux-1

--- pages/Basic/index.tsx
+++ pages/Basic/index.tsx
@@ -1,25 +1,25 @@
-import styles from "./index.module.scss";
-import { useEffect } from "react";
+import styles from './index.module.scss'
+import { useEffect } from 'react'
 // import axios from "axios";
 
 export default ({ blogs }) => {
-  //test ts安装是否成功
-  let gg: string = "ff";
-  gg = "123";
+	//test ts安装是否成功
+	let gg: string = 'ff'
+	gg = '123'
 
-  // //test api路由
-  // useEffect(() => {
-  //   const data = axios.get("./api/hello.js");
-  //   console.log(data, 123);
-  // }, []);
+	// //test api路由
+	// useEffect(() => {
+	//   const data = axios.get("./api/hello.js");
+	//   console.log(data, 123);
+	// }, []);
 
-  return (
-    <div>
-      <p className={styles.titleRed}>{blogs.title}</p>
-      <p className={styles.titleRed}>{blogs.title}</p>
-    </div>
-  );
-};
+	return (
+		<div>
+			<p className={styles.titleRed}>{blogs.title}</p>
+			<p className={styles.titleRed}>{blogs.title}</p>
+		</div>
+	)
+}
 
 //静态生成 带数据（next build）
 // export const getStaticProps = async () => {
@@ -34,13 +34,13 @@ export default ({ blogs }) => {
 
 //SSR 带数据 （外部API数据）
 export const getServerSideProps = async ({ query }) => {
-  console.log(query, "params");
+	console.log(query, 'params')
 
-  return {
-    props: {
-      blogs: {
-        title: "getServerSideProps",
-      },
-    },
-  };
-};
+	return {
+		props: {
+			blogs: {
+				title: 'getServerSideProps',
+			},
+		},
+	}
+}
--- pages/pageRedux/index.tsx
+++ pages/pageRedux/index.tsx
@@ -1,22 +1,20 @@
-import styles from "./index.module.scss";
-import { useEffect } from "react";
+import styles from './index.module.scss'
+import { useEffect } from 'react'
 
 export default ({ blogs }) => {
+	// //test api路由
+	// useEffect(() => {
+	//   const data = axios.get("./api/hello.js");
+	//   console.log(data, 123);
+	// }, []);
 
-
-  // //test api路由
-  // useEffect(() => {
-  //   const data = axios.get("./api/hello.js");
-  //   console.log(data, 123);
-  // }, []);
-
-  return (
-    <div>
-      <p className={styles.titleRed}>{blogs.title}</p>
-      <p className={styles.titleRed}>{blogs.title}</p>
-    </div>
-  );
-};
+	return (
+		<div>
+			<p className={styles.titleRed}>{blogs.title}</p>
+			<p className={styles.titleRed}>{blogs.title}</p>
+		</div>
+	)
+}
 
 //静态生成 带数据（next build）
 // export const getStaticProps = async () => {
@@ -31,13 +29,46 @@ export default ({ blogs }) => {
 
 //SSR 带数据 （外部API数据）
 export const getServerSideProps = async ({ query }) => {
-  console.log(query, "params");
+	console.log(query, 'params')
 
-  return {
-    props: {
-      blogs: {
-        title: "getServerSideProps",
-      },
-    },
-  };
-};
+	return {
+		props: {
+			blogs: {
+				title: 'getServerSideProps',
+			},
+		},
+	}
+}
+//createStore函数
+const listener = {} //触发重新渲染函数render或setState函数
+const createStore = (reducer: (s: any, a: any) => Object) => {
+	//默认
+	let state: any,
+		listeners = []
+	//获取state
+	const getState = () => state
+	//发布
+	const dispatch = (action: Object) => {
+		//1、新state
+		state = reducer(state, action)
+		// 2、更新view
+		listeners.forEach((listener) => listener())
+	}
+	//订阅
+	const subscribe = () => {
+		//1、监听
+		listeners.push(listener)
+		// 解除监听
+		return () => {
+			listeners = listeners.filter((l) => l != listener)
+		}
+	}
+	//创建store时默认设置
+	dispatch({})
+	//输出函数予以调用
+	return {
+		getState,
+		dispatch,
+		subscribe,
+	}
+}
